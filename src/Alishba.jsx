6b964e7a68d1cf4d0719c2c3a2fa6765277ef1c2
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-07-11 16:47:24 +0500

    add basics of react js

---
+++ src/Alishba.jsx
@@ -0,0 +1,11 @@
+
+function Name() {
+ 
+
+  return (
+                 <h1>My name is Alishba.</h1>
+                 
+  )
+}
+
+export default Name
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Name from './Alishba.jsx'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Name/>
+   <h3> Leraning React JS</h3>
+    </>
+  )
+}
+
+export default App
