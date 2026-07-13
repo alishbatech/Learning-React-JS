9414598a045a8c97513079a734ac8ebf0f11ee0c
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-07-13 14:40:03 +0500

    Learn React useState with counter example

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  // let counter=5;
+  let [counter,setCounter]=useState(10)
+
+      const AddValue=()=>{
+         if(counter<20){
+           setCounter( counter+1)
+          }
+          else{
+            alert('Values should not exceed then 20')
+          }
+        }
+
+       const RemoveValue=()=>{
+        if(!(counter<=0)){
+          setCounter(counter-1)
+        } 
+          else{
+            alert('Values should not below then 0')
+          }
+        }
+          
+
+  return (
+    <>
+     <h1> Learning React!</h1>
+     <h3>Counter value: {counter}</h3>
+     <button onClick={AddValue}>Add value :{counter}</button>
+     <br />
+     <button onClick={RemoveValue}>Remove value :{counter}</button>
+    </>
+  )
+}
+
+export default App
