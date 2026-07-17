8f381e9f063732e184f735afe062dc39b0c9ed54
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-07-17 15:51:12 +0500

    Props and Tailwind in React js

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -3,14 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Card from './components/card.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj={
+    age:20,
+    uni:"GCUF",
+  }
+  const myArr=["HTML","CSS","JS"]
+  const myarr=["Python","CSS","JS"]
 
   return (
     <>
           <h1 className='bg-slate-500 p-6 rounded-2xl text-white mb-4'>Tailwind Test</h1>
-          
+           <Card name="Alishba" someObj={myObj} btnText="Visit me" newArr={myArr} instock=
+           {true}/>
+    <Card name="Ayesha" newArr={myarr} instock={false}/>
     </>
   )
 }
--- 03tailwindprops/src/components/card.jsx
+++ 03tailwindprops/src/components/card.jsx
@@ -1,74 +1,87 @@
 import React from 'react'  //rfce
 
-function Card() {
+function Card({name,btnText="Click me" ,newArr, instock}) {
+     let stockStatus
+ if(instock){
+                stockStatus="Available"
+ }else{
+    stockStatus="Out of Stock"
+ }
   return (
-    
-    <div class="flex font-sans mb-5 border-2 border-red-500 rounded-2xl">
-  <div class="flex-none w-48 relative">
+    // console.log("props",newArr[0]),
+    // console.log(props.name),
+    // console.log(name),
+
+
+    <div className="flex font-sans mb-5 border-2 border-red-500 rounded-2xl">
+  <div className="flex-none w-48 relative">
     <img src="https://images.pexels.com/photos/38380902/pexels-photo-38380902.jpeg" alt="" class="absolute inset-0 w-full h-full object-cover" loading="lazy" />
   </div>
-  <form class="flex-auto p-6">
-    <div class="flex flex-wrap">
-      <h1 class="flex-auto text-lg font-semibold text-slate-900">
-        Classic Utility Jacket
+  <form className="flex-auto p-6">
+    <div className="flex flex-wrap">
+      <h1 className="flex-auto text-lg font-semibold text-slate-900">
+       {name}
       </h1>
-      <div class="text-lg font-semibold text-slate-500">
+      <div>
+        {newArr[0]}
+      </div>
+      <div className="text-lg font-semibold text-slate-500">
         $110.00
       </div>
-      <div class="w-full flex-none text-sm font-medium text-slate-700 mt-2">
-        In stock
+      <div className="w-full flex-none text-sm font-medium text-slate-700 mt-2">
+        {stockStatus}
       </div>
     </div>
-    <div class="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
-      <div class="space-x-2 flex text-sm">
+    <div className="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
+      <div className="space-x-2 flex text-sm">
         <label>
-          <input class="sr-only peer" name="size" type="radio" value="xs" checked />
-          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
-            XS
+          <input className="sr-only peer" name="size" type="radio" value="xs" checked />
+          <div className="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+        XL
           </div>
         </label>
         <label>
-          <input class="sr-only peer" name="size" type="radio" value="s" />
-          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+          <input className="sr-only peer" name="size" type="radio" value="s" />
+          <div className="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
             S
           </div>
         </label>
         <label>
-          <input class="sr-only peer" name="size" type="radio" value="m" />
-          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+          <input className="sr-only peer" name="size" type="radio" value="m" />
+          <div className="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
             M
           </div>
         </label>
         <label>
-          <input class="sr-only peer" name="size" type="radio" value="l" />
-          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+          <input className="sr-only peer" name="size" type="radio" value="l" />
+          <div className="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
             L
           </div>
         </label>
         <label>
-          <input class="sr-only peer" name="size" type="radio" value="xl" />
-          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+          <input className="sr-only peer" name="size" type="radio" value="xl" />
+          <div className="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
             XL
           </div>
         </label>
       </div>
     </div>
-    <div class="flex space-x-4 mb-6 text-sm font-medium">
-      <div class="flex-auto flex space-x-4">
-        <button class="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
-          Buy now
+    <div className="flex space-x-4 mb-6 text-sm font-medium">
+      <div className="flex-auto flex space-x-4">
+        <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+          {btnText}
         </button>
-        <button class="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
+        <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
           Add to bag
         </button>
       </div>
-      <button class="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200" type="button" aria-label="Like">
+      <button className="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200" type="button" aria-label="Like">
         <svg width="20" height="20" fill="currentColor" aria-hidden="true">
           <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
         </svg>
       </button>
     </div>
-    <p class="text-sm text-slate-700">
+    <p className="text-sm text-slate-700">
       Free shipping on all continental US orders.
     </p>
   </form>
