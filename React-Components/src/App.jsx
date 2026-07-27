c96f68f9578905d05712cadaee30d75e7e205467
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-07-27 13:44:24 +0500

    Learn React components and export methods

---
+++ React-Components/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Welcome} from './Welcome'
+import AmazingButton from './Button'
+
+function App() {
+  return (
+    <>
+    <h1>Learning Recat JS</h1>,
+    <Welcome/>,
+    <AmazingButton/>
+    </> 
+  )
+}
+
+export default App
---
+++ React-Components/src/Welcome.jsx
@@ -0,0 +1,5 @@
+const Welcome=()=>{
+  return <h2>Welcome Alishba</h2>
+}
+
+export default Welcome;
