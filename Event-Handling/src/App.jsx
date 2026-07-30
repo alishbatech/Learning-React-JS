bd33b4cd3ea7f5eab2cc2a2ea120b49bd27b9d24
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-07-30 16:17:15 +0500

    practice: event handling in react js

---
+++ Event-Handling/src/App.jsx
@@ -0,0 +1,15 @@
+import CustomButton from './CustomButton'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <CustomButton text='Like'/>
+      <CustomButton text='Comment'/>
+    </>
+  )
+}
+
+export default App
---
+++ Event-Handling/src/CustomButton.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function CustomButton({text}) {
+name="Alishba";
+    const btnhandler=(e)=>{
+        // console.log(e.target)
+        console.log(`Hey ${name}, ${text} button is clicked. `)
+    }
+    return (
+        <button onClick={btnhandler}
+            style={{
+                width: '90px',backgroundColor:'lightpink',
+                padding: '8px', margin: '50px',
+                fontSize: '18px',borderRadius:'20px'
+            }}>
+            {text}
+        </button>
+    )
+}
+
+export default CustomButton
