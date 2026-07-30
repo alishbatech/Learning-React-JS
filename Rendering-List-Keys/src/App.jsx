d57bfa8dc936b1aaa9b8178fa22fdf77344b860b
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-07-30 15:46:36 +0500

    practice:index as key in react js

--- Rendering-List-Keys/src/App.jsx
+++ Rendering-List-Keys/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import {ProductList} from './Components/ProductList'
-
+import NameList from './Components/NameList'
 function App() {
  return (
     <>
      <ProductList/>
+     <NameList/>
     </>
   )
 }
---
+++ Rendering-List-Keys/src/Components/NameList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NameList() {
+    const names=["Eleven","Will","Nancy","Mike","Mike"];
+  const  nameList=names.map((name,index)=> <h2 key={index}>{index}- {name}</h2>);
+  return (
+  <div>
+    {nameList}
+  </div>
+  )
+}
+
+export default NameList
