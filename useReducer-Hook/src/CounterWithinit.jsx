584d57cd9a36d900af575d1ed0c679716f68837f
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-08-07 19:32:24 +0500

    learn lazy intialization with useReducer

--- useReducer-Hook/src/CounterWithinit.jsx
+++ useReducer-Hook/src/CounterWithinit.jsx
@@ -1,54 +1,54 @@
 import React, { useReducer } from "react";
 
 function CounterWithinit() {
-  const initialCount = 0;
+    const initialCount = 0;
 
-  const countReducer = (state, action) => {
-    switch (action) {
-      case "increment":
-        return state + 1;
+    const countReducer = (state, action) => {
+        switch (action) {
+            case "increment":
+                return state + 1;
 
-      case "decrement":
-        return state - 1;
+            case "decrement":
+                return state - 1;
 
-      case "reset":
-        return initialCount;
+            case "reset":
+                return initialCount;
 
-      default:
-        return state;
-    }
-  };
-  const init=(initialVal)=>{
-          console.log("init function called..this only runs once.")
+            default:
+                return state;
+        }
+    };
+    const init = (initialVal) => {
+        console.log("init function called..this only runs once.")
 
-          const saveCount=localStorage.getItem("count");
-          if(saveCount !== null){
-            console.log("Found saved count:" ,saveCount)
+        const saveCount = localStorage.getItem("count");
+        if (saveCount !== null) {
+            console.log("Found saved count:", saveCount)
             return parseInt(saveCount);
-          }
-          console.log("No saved count: ",initialVal)
-          return initialVal;
-  }
+        }
+        console.log("No saved count: ", initialVal)
+        return initialVal;
+    }
 
-  const [count, dispatch] = useReducer(countReducer, initialCount,init);
+    const [count, dispatch] = useReducer(countReducer, initialCount, init);
 
-  return (
-    <div>
-      <h1>Count: {count}</h1>
+    return (
+        <div>
+            <h1>Count: {count}</h1>
 
-      <button onClick={() => dispatch("increment")}>
-        Increment
-      </button>
+            <button onClick={() => dispatch("increment")}>
+                Increment
+            </button>
 
-      <button onClick={() => dispatch("decrement")}>
-        Decrement
-      </button>
+            <button onClick={() => dispatch("decrement")}>
+                Decrement
+            </button>
 
-      <button onClick={() => dispatch("reset")}>
-        Reset
-      </button>
-    </div>
-  );
+            <button onClick={() => dispatch("reset")}>
+                Reset
+            </button>
+        </div>
+    );
 }
 
 export default CounterWithinit;
