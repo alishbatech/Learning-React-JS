570bea0eb6eba152187809c499c389e8e4a0b313
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-08-08 15:21:53 +0500

    learn React use API with context

--- useContext-Hook/src/Avatar.jsx
+++ useContext-Hook/src/Avatar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { UserContext } from './UserContext'
-import { useContext } from 'react'
+// import { useContext } from 'react'
+import { use } from 'react'
 
-function Avatar() {
-  const { user, setUser } = useContext(UserContext)
+function Avatar({isLoading=true}) {
+
+  if(!isLoading){
+   return <div>Loading user data...</div>
+  }
+  
+  const { user, setUser } = use(UserContext)
+  // const { user, setUser } = useContext(UserContext)
   const toogleTheme = () => {
     setUser({
       ...user,
@@ -17,7 +24,7 @@ function Avatar() {
       border: '2px solid skyblue', borderRadius: '40px'
     }}>
       <p>Welcome, {user.name}</p>
-      <h2>Theme: {user.theme}</h2>
+      <h2>Current Theme: {user.theme}</h2>
       <button
         style={{
           padding: '10px', backgroundColor: 'skyblue', border: 'none',
