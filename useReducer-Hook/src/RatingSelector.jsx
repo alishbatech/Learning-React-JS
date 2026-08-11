cf7b0bbbc573220b792fd0a8e719bbcd24fb1dd0
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-08-11 14:52:57 +0500

    fix: payload in RatingSelector

--- useReducer-Hook/src/RatingSelector.jsx
+++ useReducer-Hook/src/RatingSelector.jsx
@@ -1,35 +1,34 @@
 import React, { act, useReducer } from 'react'
 
-const initialstate=""
+const initialstate = ""
 
-const ratingReducer=(state,action)=>{
-    switch(action.type){
+const ratingReducer = (state, action) => {
+    switch (action.type) {
         case "SET_RATING":
-            return action.payload;
-
-         case "RESET":
+            return '✨'.repeat(action.payload.rating);
+        case "RESET":
             return initialstate;
-         default:
-          return  state      
+        default:
+            return state
     }
 
 }
 
 function RatingSelector() {
-    const [rating,dispatch]=useReducer(ratingReducer,initialstate)
-  return (
-    <div>
-        <h2>Rating Selector</h2>
-        <h2>Ratings: {rating}</h2>
-        <button onClick={()=>dispatch({type:"SET_RATING",payload:"✨"})}>1</button>
-        <button onClick={()=>dispatch({type:"SET_RATING",payload:"✨✨"})}>2</button>
-        <button onClick={()=>dispatch({type:"SET_RATING",payload:"✨✨✨"})}>3</button>
-        <button onClick={()=>dispatch({type:"SET_RATING",payload:"✨✨✨✨"})}>4</button>
-        <button onClick={()=>dispatch({type:"SET_RATING",payload:"✨✨✨✨✨"})}>5</button>
-        <button onClick={()=>dispatch({type:"RESET"})}>Reset</button>
+    const [rating, dispatch] = useReducer(ratingReducer, initialstate)
+    return (
+        <div>
+            <h2>Rating Selector</h2>
+            <h2>Ratings: {rating}</h2>
+            <button onClick={() => dispatch({ type: "SET_RATING", payload: { rating: 1 } })}>1</button>
+            <button onClick={() => dispatch({ type: "SET_RATING", payload: { rating: 2 } })}>2</button>
+            <button onClick={() => dispatch({ type: "SET_RATING", payload: { rating: 3 } })}>3</button>
+            <button onClick={() => dispatch({ type: "SET_RATING", payload: { rating: 4 } })}>4</button>
+            <button onClick={() => dispatch({ type: "SET_RATING", payload: { rating: 5 } })}>5</button>
+            <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default RatingSelector
