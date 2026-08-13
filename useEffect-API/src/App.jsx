b4263eee0b92d6f026aa2a7a9bb8485f5cd381eb
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-08-13 12:25:38 +0500

    practice: Fetching with useEffect

---
+++ useEffect-API/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import DataFetching from './DataFetching'
+
+function App() {
+ 
+  return (
+    <>
+     <h2>API in useEffect </h2>
+     <DataFetching/>
+    </>
+  )
+}
+
+export default App
