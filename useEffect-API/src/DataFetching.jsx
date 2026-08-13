748fceb2be959cf3fe5f9e2515d13889d424eeed
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-08-13 15:15:46 +0500

    add Loading and error handling in useEffect hook

--- useEffect-API/src/DataFetching.jsx
+++ useEffect-API/src/DataFetching.jsx
@@ -6,25 +6,41 @@ function DataFetching() {
     const [post,setPost]=useState({})
     const [id ,setId]=useState(1)
     const [buttonId ,setButtonId]=useState(1)
+    const [isLoading,setisLoading]=useState(false)
+    const [isError,setisError]=useState(false)
+  
 
     const handleButtonID=()=>{
         setButtonId(id);
     }
     
     useEffect(()=>{
+        setisLoading(true)
+        setisError(false)
         axios.get(`https://jsonplaceholder.typicode.com/posts/${buttonId}`)
           .then(res=>{
             console.log(res)
              setPost(res.data)
     })
-      .catch(err=>console.log(err))
+      .catch((err)=>{
+        setisError(true)
+        
+    }
+    )
+      .finally(()=>{
+        setisLoading(false)
+      })
     },[buttonId])
 
   return (
     <div>
         <input type="text" value={id} onChange={e=>setId(e.target.value)} />
         <button type='button' onClick={handleButtonID}>Fetch Data</button>
-        <h2>Title{post.id}: {post.title}</h2>
+       <h2>
+        { isLoading && <p>Loading...</p>}
+        {isError&& <p>Something went wrong</p>}
+        {!isLoading&& !isError && <p>{post.title}</p>}
+       </h2>
           {/* <ul>
             {
             posts.map((post)=>
