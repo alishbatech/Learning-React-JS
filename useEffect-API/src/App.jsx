550f114c6eeaab842b89c0b7ab199679a0c88798
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-08-15 12:17:54 +0500

    fix App component logic

--- useEffect-API/src/App.jsx
+++ useEffect-API/src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css'
 import DataFetching from './DataFetching'
-import PostSearch from './PostSearch'
+
 
 function App() {
- 
+
   return (
     <>
-     <h2>API in useEffect </h2>
-     <DataFetching/>
-     {/* <PostSearch/> */}
+      <h2>API in useEffect </h2>
+      <DataFetching />
     </>
   )
 }
