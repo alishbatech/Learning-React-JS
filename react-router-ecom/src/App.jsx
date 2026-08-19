75d65900259881107a63abe8d9b18ae6683dfd47
Alishba Shahid <alishbadeveloper999@gmail.com>
2026-08-19 13:19:57 +0500

    add react dynamic route

--- react-router-ecom/src/App.jsx
+++ react-router-ecom/src/App.jsx
@@ -1,16 +1,34 @@
-import {Routes,Route ,Link, BrowserRouter} from 'react-router-dom'
+import { Routes, Route, Link, BrowserRouter, NavLink } from 'react-router-dom'
 import Home from './Components/Home'
+import About from './Components/About'
+import Student from './Components/Student'
+import Profile from './Components/Profile'
+import StudentDetails from './Components/StudentDetails'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route  path='/' element={<Home/>}/>
-   </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <nav>
+
+
+        <NavLink to='/'>Home</NavLink>  |
+        <NavLink to='/about'>About</NavLink> |
+        <NavLink to='/student'>Student</NavLink>  |
+        <NavLink to='/profile'>Profile</NavLink>
+      </nav>
+
+
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/student' element={<Student />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/student/:id' element={<StudentDetails />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
